b077a641add226cbb7fe99fa02123cc8033624ea
bfrasure <bill.frasure@gmail.com>
2018-06-27 16:07:19 -0600

    JIRA-#### #time 10m #comment Create BrowserMockedTest

---
+++ src/test/java/com/billding/tttt/BrowserMockedTest.java
@@ -0,0 +1,29 @@
+package com.billding.tttt;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class BrowserMockedTest {
+    // TODO Pass as constructor arg
+    private static final int browserOperationRunTime = 30;
+
+    @DataProvider(name = "browsers")
+    public static Object[][] testData() {
+
+        TestInstanceCreator testInstanceCreator = new TestInstanceCreator();
+
+        return testInstanceCreator.createInstances(
+            (ignored) -> 1,
+            (idx) -> new Browser()
+        );
+
+    }
+
+    @Test(dataProvider = "browsers")
+    public void test_specific(String developer, Browser browser) {
+        assertEquals(browserOperationRunTime, browser.failableAction());
+        assertEquals(browserOperationRunTime, browser.getOperationRunTime());
+    }
+}
