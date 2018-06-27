28b1cc9a2a1b426527de74bb6e1b4c386ba3d4c6
bfrasure <bill.frasure@gmail.com>
2018-06-27 15:52:51 -0600

    JIRA-#### #time 10m #comment Create NetworkMockedTest

---
+++ src/test/java/com/billding/tttt/NetworkMockedTest.java
@@ -0,0 +1,28 @@
+package com.billding.tttt;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class NetworkMockedTest {
+    private static final int networkOperationRunTime = 1;
+
+    @DataProvider(name = "networks")
+    public static Object[][] testData() {
+
+        TestInstanceCreator testInstanceCreator = new TestInstanceCreator();
+
+        return testInstanceCreator.createInstances(
+            TestEnvironmentParameters::getNumberOfMapperTests,
+            (idx) -> new Network(networkOperationRunTime)
+        );
+
+    }
+
+    @Test(dataProvider = "networks")
+    public void test_specific(String developer, Network network) {
+        assertEquals(networkOperationRunTime, network.httpOperation(200));
+        assertEquals(networkOperationRunTime, network.getOperationRunTime());
+    }
+}
