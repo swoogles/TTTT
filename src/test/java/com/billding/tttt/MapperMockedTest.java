2d444db0d7bf7b193db9f25aa5a671382908b3fd
bfrasure <bill.frasure@gmail.com>
2018-06-27 10:41:12 -0600

    JIRA-#### #time 10m #comment Create MapperMockedTest

---
+++ src/test/java/com/billding/tttt/MapperMockedTest.java
@@ -0,0 +1,39 @@
+package com.billding.tttt;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.time.Instant;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertEquals;
+
+public class MapperMockedTest {
+    private static final int mapperOperationRunTime = 1;
+
+    @DataProvider(name = "mappers")
+    public static Object[][] primeNumbers() {
+        final ChaoticWorld chaoticWorld = mock(ChaoticWorld.class);
+        when(chaoticWorld.currentTime()).thenReturn(Instant.parse("1970-01-01T00:00:00Z"));
+        final Database database = mock(Database.class);
+        when(database.failableAction()).thenReturn(0);
+
+        TestInstanceCreator testInstanceCreator = new TestInstanceCreator();
+
+        return testInstanceCreator.createInstances(
+            TestEnvironmentParameters::getNumberOfMapperTests,
+            (idx) -> new Mapper(
+                        database,
+                        chaoticWorld,
+                        mapperOperationRunTime
+                    )
+        );
+    }
+
+    @Test(dataProvider = "mappers")
+    public void test_specific(String developer, Mapper mapper) {
+        assertEquals(mapperOperationRunTime, mapper.CRUD_query());
+        assertEquals(mapperOperationRunTime, mapper.getOperationRunTime());
+    }
+}
