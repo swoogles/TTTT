1b99c5ac32840b9b2840c52f5a18a2f0743df466
bfrasure <bill.frasure@gmail.com>
2018-07-04 23:30:32 -0600

    Add comment and swap codebase property

--- src/main/java/com/billding/meta/DemoScenarios.java
+++ src/main/java/com/billding/meta/DemoScenarios.java
@@ -2,6 +2,7 @@ package com.billding.meta;
 
 import com.billding.tttt.PropertyRetriever;
 
+// The static-ness of this file makes it harder to get logs/trackstaces about causes for failure.
 class DemoScenarios {
     private static final PropertyRetriever propertyRetriever = new PropertyRetriever("live_demo");
 
