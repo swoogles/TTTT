0c9496120cfe2eac51942c6cc4214884be8c003f
Bill Frasure <bill.frasure@gmail.com>
2018-08-07 17:38:55 -0600

    rm up arrow action

--- docs/_includes/keyboard_navigation_nonpaginated.js
+++ docs/_includes/keyboard_navigation_nonpaginated.js
@@ -5,11 +5,6 @@ function setup_keypress() {
             case 37: // left
                 document.location.href = "{{site.url}}{{page.previous.url | relative_url}}";
                 break;
-            {% endif %}
-            case 38: // up
-                document.location.href = "{{ paginator.single_page }}#{{ paginator.section_id }}";
-                break;
-            {% if page.next != nill %}
             case 39: // right
                 document.location.href = "{{site.url}}{{page.next.url | relative_url}}";
                 break;
