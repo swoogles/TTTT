9fc6b5b5dcdbac6bf493834f63def0812800d2b1
Bill Frasure <bill.frasure@gmail.com>
2018-08-08 00:11:04 -0600

    mk ServiceStatus error msgs more cryptic

--- src/main/java/com/billding/meta/ServiceStatus.java
+++ src/main/java/com/billding/meta/ServiceStatus.java
@@ -3,13 +3,13 @@ package com.billding.meta;
 import com.billding.tttt.PropertyRetriever;
 
 public class ServiceStatus {
-    // TODO ground this static shiz
+    // TODO ground this static junk
     private static PropertyRetriever propertyRetriever =
             new PropertyRetriever("service_status");
 
     public static void ensureServiceIsRunning(String name) {
         if (propertyRetriever.getBinaryBoolean(name) == false)
-            throw new RuntimeException("Good luck.");
+            throw new RuntimeException("Good luck figuring this one out.");
 //        throw new RuntimeException(name + " failed us!");
     }
 }
