75dce97725e54976771badfa22a0495420396bc0
Bill Frasure <bill.frasure@gmail.com>
2018-08-11 00:57:14 -0600

    Work on proper pagination and navigation

--- docs/_includes/keyboard_navigation_nonpaginated.js
+++ docs/_includes/keyboard_navigation_nonpaginated.js
@@ -1,14 +1,16 @@
+// This is LIKELY where the problem with my next/prev links can be fixed.
+// TODO as soon as its done here, make sure it's fixed EVERYWHERE.
 function setup_keypress() {
     document.onkeydown = function(e) {
         switch (e.keyCode) {
             {% if page.previous != nil %}
             case 37: // left
-                document.location.href = "{{site.url}}{{page.previous.url | relative_url}}";
+                document.location.href = "{{page.previous.url | relative_url}}";
                 break;
             {% endif %}
             {% if page.next != nil %}
             case 39: // right
-                document.location.href = "{{site.url}}{{page.next.url | relative_url}}";
+                document.location.href = "{{page.next.url | relative_url}}";
                 break;
             {% endif %}
         }
